1f2b57e3feea3a03876fba9997a2c17620d4087f
Vladyslav Nalyvaiko <nov.vladkyiv@gmail.com>
2026-06-12 14:17:31 +0100

    update href

--- src/lib/components/not-connected-linear.svelte
+++ src/lib/components/not-connected-linear.svelte
@@ -9,9 +9,9 @@
             <span class="text-4xl">Needline</span>
             <img src="/logo.svg" alt="Needline logo" width="54" />
         </div>
-        <Button>
+        <Button href={url}>
             <span class="flex items-center gap-2">
-                <a href={url}>Connect Linear</a>
+                <span>Connect Linear</span>
                 <img src="/linear-dark-logo.svg" alt="Linear logo" width="18" />
             </span>
         </Button>
